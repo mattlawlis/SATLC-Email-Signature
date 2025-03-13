004d76cfa07da3c1d0dbe0333673d652404d6ae7
mattlawlis <89171568+mattlawlis@users.noreply.github.com>
2025-03-12 20:45:27 -0500

    Update authorized.php

--- api/authorized.php
+++ api/authorized.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 // Optionally check if user is authenticated:
 if (empty($_SESSION['authenticated'])) {
     header('Location: /api/index.php');
